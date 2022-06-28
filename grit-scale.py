5352cb32931d01dda1e7346b9ff248fc6d5dd791
Elizabeth Schlievert <eschlievert5@gmail.com>
2022-06-27 22:03:27 -0500

    Tkinter GUI created

--- grit-scale.py
+++ grit-scale.py
@@ -1,80 +1,173 @@
-import random
+from tkinter import *
 
-def get_statements():
-    statements = []
-    statements.append(["I have overcome setbacks to conquer an important challenge.", "P"])
-    statements.append(["New ideas and projects sometimes distract me from previous ones.", "N"])
-    statements.append(["My interests change from year to year.", "N"])
-    statements.append(["Setbacks don’t discourage me.", "P"])
-    statements.append(["I have been obsessed with a certain idea or project for a short time but later lost interest.", "N"])
-    statements.append(["I am a hard worker.", "P"])
-    statements.append(["I often set a goal but later choose to pursue a different one.", "N"])
-    statements.append(["I have difficulty maintaining my focus on projects that take more than a few months to complete.", "N"])
-    statements.append(["I finish whatever I begin.", "P"])
-    statements.append(["I have achieved a goal that took years of work.", "P"])
-    statements.append(["I become interested in new pursuits every few months.", "N"])
-    statements.append(["I am diligent.", "P"])
+from tkinter import messagebox as mb
 
-    return statements
+import json
+ 
+#Class for GUI components
+class Assessment:
 
-def execute_questionnaire():
-    
-    #Get statements
-    grit_statements = get_statements()
+    def __init__(self):
+         
+        #Set statement number to 0
+        self.statement_num=0
+         
+        #Assigns statements to display_statements function to update later
+        self.display_statements()
+         
+        #Holds an integer value which is used for selected option in a question.
+        self.resp_selected=IntVar()
+         
+        #Display radio buttons
+        self.resps = self.radio_buttons()
+         
+        #Display responses
+        self.display_responses()
+         
+        #Display next button
+        self.buttons()
+         
+        #Number of statements
+        self.data_size = len(statements)
+         
+        #Counter of score
+        self.score=0
+ 
+ 
+    #Display result in message box 
+    def display_result(self, statement_num):
 
-    #Randomize statements
-    random.shuffle(grit_statements)
-
-    #Set Score
-    score = 0 
-
-    #Display statements using for loop
-
-    for s in grit_statements:
-        #Present statement
-        print(s[0])
-        
-        while True: 
-            #Request user answer
-            answer = input("a) Very much like me\nb) Mostly like me\nc) Somewhat like me\nd) Not much like me\ne) Not like me at all\n:")
-            if answer.lower() not in ("a", "b", "c", "d", "e"):
-                print(answer, "is not an appropriate choice, please select from the letters below:")
-            
-            else: 
-                break
-        
         #Calculate scoring for positively framed questions 
-        if s[1] == "P":
-            if answer.lower() == "a":
-                score = score + 5   
-            elif answer.lower() == "b":
-                score = score + 4   
-            elif answer.lower() == "c":
-                score = score + 3 
-            elif answer.lower() == "d":
-                score = score + 2 
-            elif answer.lower() == "e":
-                score = score + 1  
+        if framing[statement_num] == "P":
+            if self.resp_selected.get() == "Very much like me":
+                self.score =+ 5   
+            elif self.resp_selected.get() == "Mostly like me":
+                self.score =+ 4   
+            elif self.resp_selected.get() == "Somewhat like me":
+                self.score =+ 3 
+            elif self.resp_selected.get() == "Not much like me":
+                self.score =+ 2 
+            elif self.resp_selected.get() == "Not like me at all":
+                self.score =+ 1 
+
+        #Calculate scoring for positively framed questions 
+        if framing[statement_num] == "N":
+            if self.resp_selected.get() == "Very much like me":
+                self.score =+ 1   
+            elif self.resp_selected.get() == "Mostly like me":
+                self.score =+ 2   
+            elif self.resp_selected.get() == "Somewhat like me":
+                self.score =+ 3 
+            elif self.resp_selected.get() == "Not much like me":
+                self.score =+ 4 
+            elif self.resp_selected.get() == "Not like me at all":
+                self.score =+ 5 
         
-        #Calculate scoring for negatively framed questions 
-        if s[1] == "N":
-            if answer.lower() == "a":
-                score = score + 1   
-            elif answer.lower() == "b":
-                score = score + 2   
-            elif answer.lower() == "c":
-                score = score + 3 
-            elif answer.lower() == "d":
-                score = score + 4 
-            elif answer.lower() == "e":
-                score = score + 5  
+        #Calculate total score 
+        result = float(self.score / 12)
+         
+        #Message box to display results
+        mb.showinfo("Result", result)
+ 
+    #Show next statement
+    def next_btn(self):
+         
+        #Move to next Question by incrementing the statement_num counter
+        self.statement_num += 1
+         
+        #Check if statement_num = data size
+        if self.statement_num == self.data_size:
+             
+            # if it is correct then it displays the score
+            self.display_result()
+             
+            # destroys the GUI
+            gui.destroy()
 
-    #Calculating total Score 
-    final_score = float(score / 12)
-    print("You scored a", final_score)
+        else:
+            #Show the next question
+            self.display_statements()
+            self.display_responses()
+ 
+ 
+    #Display button on the screen
+    def buttons(self):
+         
+        #Next button 
+        next_button = Button(gui, text="Next", command=self.next_btn, width=10,font=("Arial", 16, "bold"))
+        next_button.place(x=350, y=380)
+ 
 
-def instructions():
-    print("Please respond to the following 12 statements. Be honest, there are no right or wrong answers!")
-
-instructions()
-execute_questionnaire()
+    #Display responses (next to radio buttons including select/deselect behavior)
+    def display_responses(self):
+        val=0
+         
+        #Deselect radio buttons 
+        self.resp_selected.set(0)
+         
+        #Display responses next to radio buttons 
+        for response in responses[self.statement_num]:
+            self.resps[val]['text']=response
+            val+=1
+ 
+ 
+    #Display statements 
+    def display_statements(self):
+         
+        #Statement formatting 
+        statement_num = Label(gui, text=statements[self.statement_num], width=100, font=("Arial", 16, "bold"), anchor='w')
+         
+        #Statement placement
+        statement_num.place(x=70, y=100)
+ 
+    
+    #Display radio buttons 
+    def radio_buttons(self):
+         
+        #Empty response list 
+        r_list = []
+         
+        #Response placement 
+        y_pos = 150
+         
+        #Add responses to list 
+        while len(r_list) < 5:
+             
+            #Radio button formatting 
+            radio_btn = Radiobutton(gui, text=" ", variable=self.resp_selected, value=len(r_list)+1, font=("Arial", 14))
+             
+            #Add radio button to list 
+            r_list.append(radio_btn)
+             
+            #Radio button placement 
+            radio_btn.place(x = 100, y = y_pos)
+             
+            #Increment the y-axis position by 40
+            y_pos += 40
+         
+        #Return radio buttons
+        return r_list
+ 
+#GUI window
+gui = Tk()
+ 
+#GUI window size 
+gui.geometry("1200x600")
+ 
+#GUI window title 
+gui.title("Grit Scale")
+ 
+#Snag data 
+with open('data.json') as f:
+    data = json.load(f)
+ 
+#Set statements, framing and responses 
+statements = (data['statements'])
+framing = (data['framing'])
+responses = (data['responses'])
+ 
+#Object of class
+assessment = Assessment()
+ 
+#Launch GUI
+gui.mainloop()
